724cb3c46651c1a12835c5a089e30e768bc4dc7c
Kane Shin <46589390+skh960630@users.noreply.github.com>
2019-05-17 22:17:49 +1000

    new website UI

---
+++ newUI/footer.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+  <link href="stylesheet/headstyle.css" rel="stylesheet" type="text/css">
+  <link href="stylesheet/footstyle.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <div class="borderset">
+    <center><p>Follow Us!</p></center>
+    <div class="row">
+      <a href="https://www.facebook.com/HaqCentreForChildRights">
+        <img src="images/facebook.png" style="width:30px">
+      </a>
+      <a href="https://twitter.com/haqcrc?lang=en">
+       <img src="images/twitter.png" style="width:30px">
+      </a>
+      <a href="https://plus.google.com/117835323419455080258">
+       <img src="images/google.png" style="width:30px">
+      </a>
+      <a href="https://www.youtube.com/user/haqccr">
+       <img src="images/youtube.png" style="width:30px">
+      </a>
+      <a href="https://www.linkedin.com/company/haq-centre-for-child-rights">
+       <img src="images/linkedin.png" style="width:30px">
+      </a>
+    </div>
+    <center><p>© Copyrights 2019, HAQ: Centre for Child Rights, All rights reserved</p></center>
+  </div>
+</body>
+</html>
---
+++ newUI/header.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+  <link href="stylesheet/headstyle.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <div id="container">
+    <img src="images/header.jpg" style="width:1000px;">
+    <img src="images/haqbudget.jpg" style="width:190px;" id="haqimage">
+  </div>
+  <div class="navbar">
+    <a href="main.php">Home</a>
+    <div class="dropdown">
+      <button class="dropbtn">
+        Reports
+      </button>
+      <div class="dropdown-content">
+        <a href="state.php">State</a>
+        <a href="union.php">Union</a>
+      </div>
+    </div>
+    <a href="upload.php">Upload</a>
+    <a href="aboutus.php">About Us</a>
+    <a href="faq.php">FAQ</a>
+    <a href="contact.php">Contact Us</a>
+  </div>
+</body>
+</html>
